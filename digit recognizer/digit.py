d3d38c97db34ba9ffc4cdba9970d0c4dae16d802
manzars <manzarshaikh69@gmail.com>
2019-02-22 22:31:53 +0530

    Projects Added

---
+++ digit recognizer/digit.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 20:58:48 2019
+
+@author: manzars
+"""
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv("train.csv")
+
+X = data.iloc[:, 1:].values
+y = data.iloc[:, 0:1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+classifier = RandomForestClassifier(n_estimators = 300, n_jobs = 3, criterion = "gini", random_state = 0)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+
+def check_prediction(num):
+    plt.gray()
+    plt.matshow(X_test[num].reshape(28,28))
+    plt.show()
+    
+    print("Predicted Value is %s" %(classifier.predict(X_test[num].reshape(1, -1))))
+    
+    
+def check_model_accuracy(cm):
+    sum = 0
+    for i in range(10):
+        for j in range(10):
+            if(i == j):
+                sum = sum + cm[i][j]
+    return sum / len(X_test) * 100
+
+
+
+flag = True
+while(flag):
+    choice = int(input("1. Make prediction\n2. exit\n"))
+    if(choice == 1):
+        print("Model Accuracy was %s So there can be chances of Wromg prediction" %(check_model_accuracy(cm)))
+        num = int(input("Enter the Index between (0, 8399) TO make Prediction: "))
+        check_prediction(num)
+    elif(choice == 2):
+        flag = False
+    else:
+        print("Wrong Input please try again...")
---
+++ iris_flower/iris_flower.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  9 20:06:34 2019
+
+@author: manzars
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+data = pd.DataFrame(iris.data, columns = iris.feature_names)
+data['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+data['species'] = encoder.fit_transform(data['species'])
+
+X = data.iloc[:, :4].values
+y = data.iloc[:, 4].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 100, criterion = "gini", random_state = 0)
+classifier.fit(X_train, y_train)
+
+y_pred = iris.target_names[classifier.predict(X_test)]
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
